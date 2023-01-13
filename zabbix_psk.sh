61289752414a2d38f449ee6a06059d7f6045e0af
Francisco Wesley <98036757+FWesleycosta@users.noreply.github.com>
2023-01-13 20:08:01 -0300

    Upload script for configuration key psk in zabbix-agent

---
+++ zabbix_psk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Generate PSK key
+psk=$(openssl rand -hex 32)
+
+# Create PSK key directory
+mkdir -p /etc/zabbix/psk
+
+# Write PSK key to file
+echo $psk > /etc/zabbix/psk/psk.txt
+
+# Add PSK settings to agent configuration file
+echo "TLSConnect=psk" >> /etc/zabbix/zabbix_agentd.conf
+echo "TLSPSKIdentity=$HOSTNAME" >> /etc/zabbix/zabbix_agentd.conf
+echo "TLSPSKFile=/etc/zabbix/psk/psk.txt" >> /etc/zabbix/zabbix_agentd.conf
+
+# Restart agent
+systemctl restart zabbix-agent
+
+# Print PSK key and identity name
+echo "Generated PSK key: $psk"
+echo "TLSPSKIdentity: $HOSTNAME"
