52cccf67891ffb62cf288ee6acdd4c27c2d37d52
Francisco Wesley <98036757+FWesleycosta@users.noreply.github.com>
2023-01-25 16:11:56 -0300

    Create script motd.sh

---
+++ motd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> /etc/motd
+echo "  +                                                                         +" >> /etc/motd
+echo "  +                              ### SISCOM ###                             +" >> /etc/motd
+echo "  +                                                                         +" >> /etc/motd
+echo "  +             ACESSO PERMITIDO SOMENTE A PESSOAS AUTORIZADAS              +" >> /etc/motd
+echo "  +      TODAS AS CONEXOES ESTAO SENDO LOGADAS, MONITORADAS E AUDITADAS     +" >> /etc/motd
+echo "  +                  ATTENTION: AUTHORIZED PERSONAL ONLY.                   +" >> /etc/motd
+echo "  +                    IF YOU ARE NOT AUTHORIZED PERSONAL                   +" >> /etc/motd
+echo "  +                         DISCONNECT IMMEDIATELY.                         +" >> /etc/motd
+echo "  +                                                                         +" >> /etc/motd
+echo "  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> /etc/motd
